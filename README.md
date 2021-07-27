31da362f35fadde2ed07aa8ed377b789474dbd9a
wchetty33 <wchetty33@gmail.com>
2021-07-27 12:41:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-sga8gt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-sga8gt)
---
+++ src/mock-heroes/mock-heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+
+@Component({
+  selector: 'app-mock-heroes',
+  templateUrl: './mock-heroes.component.html',
+  styleUrls: ['./mock-heroes.component.css']
+})
+export class MockHeroesComponent implements OnInit {
+  export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
